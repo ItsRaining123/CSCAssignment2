ae4e72f37079cd23bd62ba2a17abd43d71eeb357
Jov <jovianlim.17@ichat.sp.edu.sg>
2020-08-03 16:02:08 +0800

    Recommendations

---
+++ CSCAssignment2/Controllers/RecommendationsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSCAssignment2.Controllers
+{
+    public class RecommendationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddUserRecommendation()
+        {
+            return View();
+        }
+        public IActionResult AddItemRecommendation()
+        {
+            return View();
+        }
+    }
+}
